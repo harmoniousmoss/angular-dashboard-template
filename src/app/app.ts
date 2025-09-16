1907dd4e5e64c90eb64aa3cea2e54db8426b1ed3
Syb <99127787+harmoniousmoss@users.noreply.github.com>
2025-09-16 10:14:41 +0700

    Remove unused SidebarComponent import to fix warning

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './sidebar/sidebar';
 import { DashboardComponent } from './dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, DashboardComponent],
+  imports: [RouterOutlet, DashboardComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
